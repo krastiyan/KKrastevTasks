5b9f69e44709a9f5cd557a79705ad866163bb540
Krastiyan <krastian@gmail.com>
2016-11-09 11:12:25 +0200

    Dictionary reading text from console and counting number of mentions of each word in text.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> textDictionary = new Dictionary<string, int>();
+            Console.WriteLine("Enter your text:");
+            string[] textInput = Console.ReadLine().Split(new string[] { " ", ",", ".", "!", "?", ";", ":", "(", ")", "[", "]", "<", ">"}, StringSplitOptions.RemoveEmptyEntries);
+            for (int i=0;i<textInput.Length;i++)
+            {
+                if (textDictionary.ContainsKey(textInput[i]))
+                {
+                    textDictionary[textInput[i]]++;
+                }
+                else
+                {
+                    textDictionary.Add(textInput[i], 1);
+                }
+            }
+
+            Console.WriteLine("\nDone reading\n");
+            foreach(string key in textDictionary.Keys)
+            {
+                Console.WriteLine($"Number of mentions of {key} = {textDictionary[key]}");
+            }
+        }
+    }
+}
